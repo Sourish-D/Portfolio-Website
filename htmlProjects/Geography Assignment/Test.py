144fa67afe9a6ee510eabbbb34312988e28e19d3
Sourish-D <sourish.dhakal1@gmail.com>
2026-07-12 16:27:11 -0400

    Adding Certificates and HTML/CSS/JS Projects page

---
+++ htmlProjects/Geography Assignment/Test.py
@@ -0,0 +1,13 @@
+#File
+thing = {"Yes":"Yes","Yes2":2}
+print(thing["Yes2"])
+for i in "bruh":
+    print(i)
+print("bruh"[0])
+multilinestring = '''
+    Multi
+    Line
+    String
+    Amazing
+'''
+print(multilinestring)
