929a8186a451fc070a682d957a1639a919d729f5
Sourish-D <sourish.dhakal1@gmail.com>
2026-08-09 21:51:15 -0400

    Adding initial components in home to ensure react app is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react';
-import {BrowswerRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './index.css'
 
-import Home from './pages/Home';
-import About from './pages/About';
-import ProjectsPage from './pages/ProjectPage';
+import Home from './pages/Home.jsx';
+import Background from './pages/Background.jsx';
+import ProjectsPage from './pages/ProjectsPage.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/background" element={<Background />} />
         <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../index.css";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Hey, I'm Sourish!</h1>
+      <p>Welcome to my personal website.</p>
+    </main>
+  )
+}
+
+export default Home
